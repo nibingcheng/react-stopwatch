8a098ccfa739a7232a2d0363a88696b16d4cebd3
Bingcheng Ni <nibingcheng@johndeere.com>
2021-02-17 18:23:32 -0600

    Count up functional

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      counter: 0
+    }
+  }
+    
+  handleStart=()=> {
+    setInterval(this.increaseCounter, 1000);
+  }
+  
+  increaseCounter=()=> {
+    this.setState ({
+          counter: this.state.counter + 1
+    })
+    // this.state.counter++;
+    console.log("hello", this.state.counter);
+  }
+  
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.counter}</h1>
         <div className="controls">
           <button>Reset</button>
-          <button>Start</button>
+          <button onClick={this.handleStart}>Start</button>
           <button>Pause</button>
         </div>
       </div>
